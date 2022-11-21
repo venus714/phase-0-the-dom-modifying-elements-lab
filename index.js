7a804b1ad3365c77f38059b5f65f824bdfa5a6f2
Student <venus.kabera@student.moringaschool.com>
2022-11-21 09:43:32 +0300

    comleted assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code here!
+const main = document.getElementById("main").remove();
+const newHeader=document.createElement("h1");
+newHeader.id='victory';
+newHeader.innerHTML="Venus is the champion";
